fc766dc3cc586efe845637cb4eb64d0deea3580b
Ret-Mode <ernest.harcej@wp.pl>
2023-09-19 09:45:11 +0200

    Added simple texture shader

--- test.py
+++ test.py
@@ -9,6 +9,112 @@ import time
 from typing import List
 
 
+class TextureDraw:
+
+    def __init__(self, ctx:ArcadeContext):
+        vertexShader="""
+            #version 330
+
+            in vec2 inVert;
+            in vec2 inUV;
+
+            out vec2 fUV;
+
+            void main() {
+                fUV = inUV;
+                gl_Position = vec4(inVert.xy, 0.0, 1.0);
+            }
+            """
+
+        fragmentShader="""
+            #version 330
+
+            in vec2 fUV;
+
+            uniform sampler2D t1;
+            uniform sampler2D t2;
+
+            void main() {
+                gl_FragColor = mix(texture(t1, fUV), texture(t2, fUV), 0.5);
+            }
+            """
+        self.program = ctx.program(vertex_shader=vertexShader, fragment_shader=fragmentShader)
+
+        verts = array.array('f', [
+                        -0.5, -0.5,
+                        0.5, -0.5,
+                        0.0, 0.5
+        ])
+        
+        self.verts = ctx.buffer(data=verts)
+        vertsDescription = BufferDescription(self.verts, '2f', ['inVert'] )
+
+        uvs = array.array('f', [0.0, 0.0,
+                                   1.0, 0.0,
+                                   0.5, 1.0])
+        self.uvs = ctx.buffer(data=uvs)
+        colorsDescription = BufferDescription(self.uvs, '2f', ['inUV'])
+
+        indices = array.array('I', [0,1,2])
+        self.indices = ctx.buffer(data=indices)
+
+        self.texture1 = ctx.load_texture("pinky.png")
+        self.texture2 = ctx.load_texture("wheel2.png")
+
+        self.program.set_uniform_safe('t1', 0)
+        self.program.set_uniform_safe('t2', 1)
+
+        self.geometry = ctx.geometry([vertsDescription, colorsDescription], 
+                                     index_buffer=self.indices, 
+                                     mode=ctx.TRIANGLES)
+        ctx.disable(pyglet.gl.GL_DEPTH_TEST)
+
+
+
+    def updateIndices(self, indices:List[int]):
+        indicesInBytes = len(indices) * 4
+        if indicesInBytes != self.indices.size:
+            self.indices.orphan(size=indicesInBytes)
+        self.indices.write(array.array('I', indices))
+        self.geometry.num_vertices = len(indices)
+
+    def updateVerts(self, verts:List[float]):
+        vertsInBytes = len(verts) * 4
+        if vertsInBytes > self.verts.size:
+            self.verts.orphan(size=vertsInBytes)
+        self.verts.write(array.array('f', verts))
+
+    def updateUVs(self, uvs:List[float]):
+        uvsInBytes = len(uvs) * 4
+        if uvsInBytes > self.uvs.size:
+            self.uvs.orphan(size=uvsInBytes)
+        self.uvs.write(array.array('f', uvs))
+
+    def update(self, verts:List[float], uvs:List[float], indices:List[int]):
+        vertsInBytes = len(verts) * 4
+        uvsInBytes = len(uvs) * 4
+        indicesInBytes = len(indices) * 4
+
+        if vertsInBytes > self.verts.size:
+            self.verts.orphan(size=vertsInBytes)
+        self.verts.write(array.array('f', verts))
+        
+        if uvsInBytes > self.uvs.size:
+            self.uvs.orphan(size=uvsInBytes)
+        self.uvs.write(array.array('f', uvs))
+        
+        if indicesInBytes != self.indices.size:
+            self.indices.orphan(size=indicesInBytes)
+            self.geometry.num_vertices = len(indices)
+        self.indices.write(array.array('I', indices))
+
+    def draw(self):
+        self.texture1.use(0)
+        self.texture2.use(1)
+        #self.updateIndices([random.randint(0,2) for i in range(30)])
+        self.geometry.render(self.program)
+
+
 class LineDraw:
 
     def __init__(self, ctx:ArcadeContext):
@@ -112,10 +218,12 @@ class Runner(arcade.Window):
 
     def __init__(self, width, height, title):
         super().__init__(width, height, title, resizable=True)
-        self.lineDraw = LineDraw(self.ctx)
+        self.lineDraw = TextureDraw(self.ctx)
+        self.sprite = arcade.Sprite("./pinky.png", 1.0)
 
     def on_draw(self):
         self.clear()
+        self.sprite.draw()
         self.lineDraw.draw()
 
 
